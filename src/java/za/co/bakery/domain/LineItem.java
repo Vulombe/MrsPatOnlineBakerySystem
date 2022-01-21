7535b9244ffd4570f4a4bdd432b6fad6bf02bf53
OmriCass <82949255+OmriCass@users.noreply.github.com>
2022-01-21 12:22:15 +0200

    Omri - added ID to lineitem

--- src/java/za/co/bakery/domain/LineItem.java
+++ src/java/za/co/bakery/domain/LineItem.java
@@ -7,10 +7,19 @@ package za.co.bakery.domain;
 public class LineItem {
     private Product product;
     private int qty;
+    private int ingredientId;
 
     public LineItem(Product product, int qty) {
         this.product = product;
         this.qty = qty;
+    }
+
+    public int getIngredientId() {
+        return ingredientId;
+    }
+
+    public void setIngredientId(int ingredientId) {
+        this.ingredientId = ingredientId;
     }
 
     public Product getProduct() {
