eaaca2d1f44b9351d85bac8b9688f581a8591483
Vulombe <vmakhubele@gmail.com>
2022-01-21 11:44:04 +0200

    Added Order Domain and UserAddress Domain

--- src/java/za/co/bakery/domain/Order.java
+++ src/java/za/co/bakery/domain/Order.java
@@ -1,16 +1,36 @@
 package za.co.bakery.domain;
 
+import java.util.List;
+
 public class Order {
 
+    private int orderID;
     private User user;
+    private List<LineItem> lineItem;
     private UserAddress custAddress;
     private double totalPrice;
-
-    public Order(User user, UserAddress custAddress, double totalPrice) {
+    
+    public Order(User user, UserAddress custAddress, double totalPrice,List<LineItem> lineItem) {
         this.user = user;
         this.custAddress = custAddress;
         this.totalPrice = totalPrice;
+        this.lineItem = lineItem;
+    }
 
+    public int getOrderID() {
+        return orderID;
+    }
+
+    public void setOrderID(int orderID) {
+        this.orderID = orderID;
+    }
+
+    public List<LineItem> getLineItem() {
+        return lineItem;
+    }
+
+    public void setLineItem(List<LineItem> lineItem) {
+        this.lineItem = lineItem;
     }
 
     public User getUser() {
